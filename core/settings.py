142834a48f4c858c26d51ec1298b2a1add722fba
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-20 16:21:55 -0400

    successful docker image/container creation implmenting mysql database

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4j_!l8)cj%u3^tt(p*y*!!rvb@=9f-93jgwk^wf*+bm9rd0$x!'
+SECRET_KEY = 'django-insecure-qvvgp3q*)(3gyo)+m&92d=^(q(hedgxdcx(g&pc&veh_277tp4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
